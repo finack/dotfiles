7a4f0e6fe32d7775c630708559fcb385cdf6da91
Peter Baker <peter@finack.com>
2013-11-04 20:59:45 -0800

    zsh alias for setting up tmux pairing

--- zsh/aliases/ruby.zsh
+++ zsh/aliases/ruby.zsh
@@ -3,3 +3,11 @@ alias bi="bundle install"
 alias bl="bundle list"
 alias bp="bundle package"
 alias bu="bundle update"
+
+# Ruby
+alias help-strftime='ruby ~/bin/help-strftime.rb'
+
+# Rails
+alias r='bundle exec rails'
+alias migrate="bundle exec db:migrate db:test:prepare"
+alias remigrate="bundle exec db:migrate db:migrate:redo db:schema:dump db:test:prepare"
