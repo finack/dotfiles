07f31c7c3744a7913c1b2fd38264c441cf075322
Peter Baker <peter@finack.com>
2013-12-14 17:18:12 -0800

    Knife Alias cleanup

---
+++ zsh/aliases/knife.zsh
@@ -0,0 +1,23 @@
+alias knife='bundle exec knife'
+
+alias ke='knife environment'
+alias kes='ke show'
+
+alias kc='knife cluster'
+alias kcl='kc list'
+alias kcs='kc show'
+alias kcst='kc status'
+
+alias ki='knife instance'
+alias kil='ki list'
+alias kis='ki show'
+
+alias kn='knife node'
+alias knl='kn list'
+alias knd='kn delete'
+alias kne='kn edit'
+alias kns='kn show'
+
+alias kdb='knife data bag'
+alias kdbe='kn edit'
+alias kdbes='kdbe --secret-file ~/.secrets/zestcash/development/application_keys/chef_development_databag.key'
