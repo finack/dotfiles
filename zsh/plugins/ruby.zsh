41277cc1a78369a2206820eb130f5d9c27ae0bb1
Peter Baker <peter@finack.com>
2014-10-07 18:20:46 -0700

    Added vim-go

--- zsh/plugins/ruby.zsh
+++ zsh/plugins/ruby.zsh
@@ -3,4 +3,7 @@
 # [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 # [[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"
 
+[[ -s "$HOME/.rbenv/bin/rbenv" ]] && export PATH=$PATH:$HOME/.rbenv/bin
+export PATH="$(consolidate-path)"
+
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
