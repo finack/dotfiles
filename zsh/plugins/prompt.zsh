07a830dcaec6ba7ff0f48ac7dc93d609e0a1177a
Peter Baker <peter@finack.com>
2021-01-01 15:06:08 -0800

    Powerlevel10k prompt

--- zsh/plugins/prompt.zsh
+++ zsh/plugins/prompt.zsh
@@ -18,26 +18,11 @@ BASE16_SHELL="$HOME/.dotfiles/themes/base16-shell/"
 #expand functions in the prompt
 setopt prompt_subst
 
-#https://github.com/olivierverdier/zsh-git-prompt
-source ~/.zsh/zsh-git-prompt/zshrc.sh
-
-# expand functions in the prompt
-setopt prompt_subst
-
 # prompt
 local ssh_prompt="%F{064}%n@%m%f:"
-local MODE_INDICATOR="%F{136}-%fvi-cmd%F{136}-%f"
+# local MODE_INDICATOR="%F{136}-%fvi-cmd%F{136}-%f"
 
 # Set path to red if return code is not 0
 export PROMPT="${SSH_CONNECTION+${ssh_prompt}}%(?,%F{136},%F{160})%~%f "
-export RPROMPT='$(git_super_status)'
 
-function zle-line-init {
-  zle reset-prompt
-}
-function zle-line-init zle-keymap-select {
-  export RPROMPT="${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/$(git_super_status)}"
-  zle reset-prompt
-}
-zle -N zle-line-init
 zle -N zle-keymap-select
