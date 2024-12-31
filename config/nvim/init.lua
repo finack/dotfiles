2b3d8a20ae95abcc500509981f99405748f2093e
Peter Baker <peter@blaide.com>
2024-12-30 17:24:34 -0800

    Remove my bespoke vim setup

--- config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- Bootstrapped and copied from https://gitlab.com/oinak/dot_config_nvim
-
-require("config.options")
-require("config.mappings")
-require("config.packages")
-require("config.diagnostics")
