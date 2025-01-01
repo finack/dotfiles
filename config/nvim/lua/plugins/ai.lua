829ee64faa876bc6c55ef3995a526319617d5fff
Peter Baker <peter@blaide.com>
2024-12-31 17:06:35 -0800

    Add code complete

---
+++ config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = {
+            adapter = "anthropic",
+          },
+          inline = {
+            adapter = "anthropic",
+          },
+        },
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {})
+          end,
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Chat (CodeCompanion)", mode = { "n", "v" } },
+      { "<leader>ai", "<cmd>CodeCompanionActions<cr>", desc = "Actions (CodeCompanion)", mode = { "n", "v" } },
+      { "ga", "<cmd>CodeCompanionChat Add<cr>", desc = "Add to AI context (CodeCompanion)", mode = "v" },
+    },
+  },
+
+  -- Bink integration
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- default = { "lsp", "path", "buffer", "codecompanion" },
+        default = { "codecompanion" },
+        providers = {
+          codecompanion = {
+            name = "CodeCompanion",
+            module = "codecompanion.providers.completion.blink",
+            enabled = true,
+          },
+        },
+      },
+    },
+  },
+
+  -- Edgy integration
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "codecompanion",
+        title = "CodeCompanion Chat",
+        size = { width = 50 },
+      })
+    end,
+  },
+
+  -- lualine integration
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(
+        opts.sections.lualine_x,
+        2,
+        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
+    end,
+  },
+}
