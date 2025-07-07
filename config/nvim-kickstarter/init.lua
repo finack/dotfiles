a3ed171c3701b97c86359147ed22af67a07997c8
Peter Baker <peter@blaide.com>
2025-07-07 08:44:44 -0700

    Use new config by default

--- config/nvim-kickstarter/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-
-require 'options'
-require 'keymaps'
-require 'lazy-bootstrap'
-require 'lazy-plugins'
--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,113 +1,93 @@
 return {
   {
-    "olimorris/codecompanion.nvim",
+    'olimorris/codecompanion.nvim',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
-      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
+      { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
     },
     config = function()
-      require("codecompanion").setup({
+      require('codecompanion').setup {
         strategies = {
           chat = {
             slash_commands = {
-              ["buffer"] = {
+              ['buffer'] = {
                 opts = {
-                  provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                  provider = 'fzf_lua', -- default|telescope|mini_pick|fzf_lua
                 },
               },
 
-              ["file"] = {
+              ['file'] = {
                 opts = {
-                  provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                  provider = 'fzf_lua', -- default|telescope|mini_pick|fzf_lua
                 },
               },
 
-              ["help"] = {
+              ['help'] = {
                 opts = {
-                  provider = "fzf_lua", -- telescope|mini_pick|fzf_lua
+                  provider = 'fzf_lua', -- telescope|mini_pick|fzf_lua
                 },
               },
 
-              ["symbols"] = {
+              ['symbols'] = {
                 opts = {
-                  provider = "fzf_lua", -- default|telescope|mini_pick|fzf_lua
+                  provider = 'fzf_lua', -- default|telescope|mini_pick|fzf_lua
                 },
               },
             },
-            adapter = "anthropic",
+            adapter = 'anthropic',
           },
           inline = {
-            adapter = "anthropic",
+            adapter = 'anthropic',
           },
         },
         adapters = {
           anthropic = function()
-            return require("codecompanion.adapters").extend("anthropic", {})
+            return require('codecompanion.adapters').extend('anthropic', {})
           end,
         },
-      })
+      }
     end,
     keys = {
-      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Chat (CodeCompanion)", mode = { "n", "v" } },
-      { "<leader>ai", "<cmd>CodeCompanionActions<cr>", desc = "Actions (CodeCompanion)", mode = { "n", "v" } },
-      { "ga", "<cmd>CodeCompanionChat Add<cr>", desc = "Add to AI context (CodeCompanion)", mode = "v" },
+      { '<leader>ac', '<cmd>CodeCompanionChat Toggle<cr>', desc = 'Chat (CodeCompanion)', mode = { 'n', 'v' } },
+      { '<leader>ai', '<cmd>CodeCompanionActions<cr>', desc = 'Actions (CodeCompanion)', mode = { 'n', 'v' } },
+      { 'ga', '<cmd>CodeCompanionChat Add<cr>', desc = 'Add to AI context (CodeCompanion)', mode = 'v' },
     },
   },
 
   -- Bink integration
   {
-    "saghen/blink.cmp",
+    'saghen/blink.cmp',
     opts = {
       sources = {
         -- default = { "lsp", "path", "buffer", "codecompanion" },
-        default = { "codecompanion" },
+        default = { 'codecompanion' },
         providers = {
           codecompanion = {
-            name = "CodeCompanion",
-            module = "codecompanion.providers.completion.blink",
+            name = 'CodeCompanion',
+            module = 'codecompanion.providers.completion.blink',
             enabled = true,
           },
         },
       },
     },
     opts_extend = {
-      "sources.default",
+      'sources.default',
     },
   },
 
   -- Edgy integration
   {
-    "folke/edgy.nvim",
+    'folke/edgy.nvim',
     optional = true,
     opts = function(_, opts)
       opts.right = opts.right or {}
       table.insert(opts.right, {
-        ft = "codecompanion",
-        title = "CodeCompanion Chat",
+        ft = 'codecompanion',
+        title = 'CodeCompanion Chat',
         size = { width = 50 },
       })
     end,
   },
-
-  -- lualine integration
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   optional = true,
-  --   event = "VeryLazy",
-  --   opts = function(_, opts)
-  --     table.insert(
-  --       opts.sections.lualine_x,
-  --       2,
-  --       LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
-  --         local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
-  --         if #clients > 0 then
-  --           local status = require("copilot.api").status.data.status
-  --           return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
-  --         end
-  --       end)
-  --     )
-  --   end,
-  -- },
 }
